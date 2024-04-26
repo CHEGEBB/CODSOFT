3564679d698e029f28edc052eb102eb36cf6e75f
CHEGEBB <chegephil24@gmail.com>
2024-04-26 15:39:03 +0300

    react

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Contact from './components/Contact';
+import Products from './components/Products';
+import Shop from './components/Shop';
+import User from './components/User';
+import Checkout from './components/Checkout';
+import Cart from './components/Cart';
+import Login from './components/Login';
 
-
-function App() {
-
-  return (
-    <div className="App">
-
-    </div>
-  )
+const App = () => {
+    return (
+        <Router>
+            <div className="App">
+                <Navbar />
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route path="/contact" component={Contact} />
+                    <Route path="/products" component={Products} />
+                    <Route path="/shop" component={Shop} />
+                    <Route path="/user" component={User} />
+                    <Route path="/checkout" component={Checkout} />
+                    <Route path="/cart" component={Cart} />
+                    <Route path="/login" component={Login} />
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
-export default App
+export default App;
