1e34d2a699c89a453044af51d15db4d8eba907af
CHEGEBB <chegephil24@gmail.com>
2024-04-27 10:42:05 +0300

    scss

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import '../scss/Home.scss';
 
 const Home = () => {
     return (
         <div>
-        <h1>Trial</h1>
-            {/* Home page content */}
+        <div className="header">
+            <div className="container">
+                <h1>Welcome to My Website</h1>
+                <p>Your one-stop shop for all things awesome.</p>
+            </div>
+        </div>
         </div>
     );
 }
