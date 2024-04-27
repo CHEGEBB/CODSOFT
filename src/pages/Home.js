ff14b1d8f731243e3c4528c845f70a601873efdf
CHEGEBB <chegephil24@gmail.com>
2024-04-27 20:09:15 +0300

    scss

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,152 +1,133 @@
 import React, { useEffect, useState } from 'react';
-import './Home.scss'
-
+import './Home.scss';
 import pic2 from '../images/pic2.jpg';
 import wom2 from '../images/pic3.jpg';
 import sneakers3 from '../images/js.jfif';
+import CartIcon from "../images/ic--round-shopping-cart.svg";
+import WishlistIcon from "../images/icon-park-solid--love-and-help.svg";
 
 const images = [
     {
-      src: pic2,
-      text: 'First Image',
-      heading: 'Discover Your Style',
-      paragraph: 'Explore our latest collection and find the perfect outfit that reflects your unique personality',
-      buttonText: 'Shop Now'
+        src: pic2,
+        text: 'First Image',
+        heading: 'Discover Your Style',
+        paragraph: 'Explore our latest collection and find the perfect outfit that reflects your unique personality',
+        buttonText: 'Shop Now'
     },
     {
-      src: wom2,
-      text: 'Second Image',
-      heading: 'Elevate Your Wardrobe',
-      paragraph: 'Revamp your closet with our exclusive selection of trendy apparel',
-      buttonText: 'Shop Now'
+        src: wom2,
+        text: 'Second Image',
+        heading: 'Elevate Your Wardrobe',
+        paragraph: 'Revamp your closet with our exclusive selection of trendy apparel',
+        buttonText: 'Shop Now'
     },
     {
-      src: sneakers3,
-      text: 'Third Image',
-      heading: 'Step into Comfort',
-      paragraph: 'Experience unparalleled comfort and style with our premium footwear collection',
-      buttonText: 'Shop Now'
+        src: sneakers3,
+        text: 'Third Image',
+        heading: 'Step into Comfort',
+        paragraph: 'Experience unparalleled comfort and style with our premium footwear collection',
+        buttonText: 'Shop Now'
     }
-  ];
-  
+];
 
 const Home = () => {
-  const [index, setIndex] = useState(0);
+    const [index, setIndex] = useState(0);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 5000);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIndex((prevIndex) => (prevIndex + 1) % images.length);
+        }, 5000);
 
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);
 
-  useEffect(() => {
-    const elements = document.querySelectorAll('.animate');
-    elements.forEach((element) => {
-      element.classList.remove('fadeIn');
-      void element.offsetWidth;
-      element.classList.add('fadeIn');
-    });
-  }, [index]);
+    useEffect(() => {
+        const elements = document.querySelectorAll('.animate');
+        elements.forEach((element) => {
+            element.classList.remove('fadeIn');
+            void element.offsetWidth;
+            element.classList.add('fadeIn');
+        });
+    }, [index]);
 
-  return (
-    <div className="Homepage">
-      <div
-        className="home-intro"
-        style={{
-          backgroundImage: `url(${images[index].src})`,
-          transition: 'background-image 1s ease-in-out'
-        }}
-      >
-        <div className="text-container animate">
-          <h1 className="heading animate">{images[index].heading}</h1>
-          <p className="paragraph animate">{images[index].paragraph}</p>
-          <p className="colored-paragraph animate">{images[index].paragraph}</p>
-          <button className="animate">{images[index].buttonText}</button>
-        </div>
-      </div>
-      <div className="loading-indicator">
-        {[0, 1, 2].map((i) => (
-          <span
-            key={i}
-            className={i === index ? 'dot active' : 'dot'}
-          ></span>
-        ))}
-      </div>
-      <div className="home-content">
-        <div className="women-shop">
-        <h1>Shop Women's</h1>
-          <button>Shop Now</button>
-        </div>
-        <div className="mens-shop">
-        <h1>Shop Men's</h1>
-          <button>Shop Now</button>
-        </div>
-      </div>
-      <div className="welcome">
-        <h1>Welcome to GlamourGallerie</h1>
-        <p>Discover the latest fashion trends and elevate your wardrobe with Glamour Gallerie. Our exclusive collection of apparel, footwear, and accessories is designed to help you make a statement wherever you go. Explore our online store and find the perfect outfit that reflects your unique personality.</p>
-        <div className="items">
-            <div className="dresses">
-                <h3>Dresses</h3>
-                <button>Shop Now</button>
-            </div>
-            <div className="watches">
-                <h3>Watches</h3>
-                <button>Shop Now</button>
-            </div>
-            <div className="shoes">
-                <h3>Shoes</h3>
-                <button>Shop Now</button>
+    return (
+        <div className="Homepage">
+            <div
+                className="home-intro"
+                style={{
+                    backgroundImage: `url(${images[index].src})`,
+                    transition: 'background-image 1s ease-in-out'
+                }}
+            >
+                <div className="text-container animate">
+                    <h1 className="heading animate">{images[index].heading}</h1>
+                    <p className="paragraph animate">{images[index].paragraph}</p>
+                    <p className="colored-paragraph animate">{images[index].paragraph}</p>
+                    <button className="animate">{images[index].buttonText}</button>
                 </div>
-                <div className="accessories">
-                <h3>Accessories</h3>
-                <button>Shop Now</button>
+            </div>
+            <div className="loading-indicator">
+                {[0, 1, 2].map((i) => (
+                    <span
+                        key={i}
+                        className={i === index ? 'dot active' : 'dot'}
+                    ></span>
+                ))}
+            </div>
+            <div className="home-content">
+                <div className="women-shop">
+                    <h1>Shop Women's</h1>
+                    <button>Shop Now</button>
                 </div>
+                <div className="mens-shop">
+                    <h1>Shop Men's</h1>
+                    <button>Shop Now</button>
+                </div>
+            </div>
+            <div className="welcome">
+                <h1>Welcome to GlamourGallerie</h1>
+                <p>Discover the latest fashion trends and elevate your wardrobe with Glamour Gallerie. Our exclusive collection of apparel, footwear, and accessories is designed to help you make a statement wherever you go. Explore our online store and find the perfect outfit that reflects your unique personality.</p>
+                <div className="items">
+                    <div className="dresses">
+                        <h3>Dresses</h3>
+                        <button>Shop Now</button>
+                    </div>
+                    <div className="watches">
+                        <h3>Watches</h3>
+                        <button>Shop Now</button>
+                    </div>
+                    <div className="shoes">
+                        <h3>Shoes</h3>
+                        <button>Shop Now</button>
+                    </div>
+                    <div className="accessories">
+                        <h3>Accessories</h3>
+                        <button>Shop Now</button>
+                    </div>
+                </div>
+            </div>
+            <div className="featured">
+                <h1>Featured Products</h1>
+                <div className="products-row1">
+                    {images.map((image, idx) => (
+                        <div className="product" key={idx}>
+                            <h3>{image.heading}</h3>
+                            <p>$99.99</p>
+                            <div className="button-container">
+                                <button>Add to Cart</button>
+                                <div className="icon-container">
+                                    <img src={CartIcon} alt="Cart Icon" />
+                                    <img src={WishlistIcon} alt="Wishlist Icon" />
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
-      </div>
-      <div className="featured">
-        <h1>Featured Products</h1>
-        <div className="products-row1">
-            <div className="product">
-                <h3>Product Name</h3>
-                <p>$99.99</p>
-                <button>Add to Cart</button>
-            </div>
-            <div className="product">
-                <h3>Product Name</h3>
-                <p>$99.99</p>
-                <button>Add to Cart</button>
-            </div>
-            <div className="product">
-                <h3>Product Name</h3>
-                <p>$99.99</p>
-                <button>Add to Cart</button>
-            </div>
-        </div>
-        <div className="products-row2">
-            <div className="product">
-                <h3>Product Name</h3>
-                <p>$99.99</p>
-                <button>Add to Cart</button>
-            </div>
-            <div className="product">
-                <h3>Product Name</h3>
-                <p>$99.99</p>
-                <button>Add to Cart</button>
-            </div>
-            <div className="product">
-                <h3>Product Name</h3>
-                <p>$99.99</p>
-                <button>Add to Cart</button>
-            </div>
-      </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Home;
