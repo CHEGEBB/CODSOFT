3be981ffff0cd99b1193573b9fa8f90d8b103fe0
CHEGEBB <chegephil24@gmail.com>
2024-04-27 12:49:15 +0300

    scss

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,12 +4,14 @@ import '../index.scss';
 const Home = () => {
     return (
         <div className="Homepage">
-        <div className="header">
-            <div className="container">
-                <h1>Welcome to My Website</h1>
-                <p>Your one-stop shop for all things awesome.</p>
-            </div>
+        <div className="home-intro">
+            <h1>Welcome to Glamour Gallerie</h1>
+            <p>Your one stop shop for all things beauty</p>
         </div>
+        <div className="home-cta">
+            <h2>Shop Now</h2>
+            <button>Shop</button>
+            </div>
         </div>
     );
 }
