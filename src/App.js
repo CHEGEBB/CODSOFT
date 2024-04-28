4dca479b01baa7e751c58b8690e4bfaca7fbbe76
CHEGEBB <chegephil24@gmail.com>
2024-04-28 18:01:12 +0300

    scss

--- src/App.js
+++ src/App.js
@@ -13,21 +13,43 @@ import CartIcon from "./images/ic--round-shopping-cart.svg";
 import Wishlist from "./images/icon-park-solid--love-and-help.svg";
 import SearchIcon from "./images/ant-design--search-outlined.svg";
 import FilterIcon from "./images/bx--filter.svg";
+import CallIcon from "./images/ic--round-call.svg";
+import MailIcon from "./images/mdi--email.svg";
+import PersonIcon from "./images/mdi--person.svg";
 
 
 const App = () => {
   return (
     <Router>
       <div className="header">
-        <div className="phone">
-          <p>Phone: 123-456-7890</p>
+      <div className="reach">
+      <div className="phone">
+      <div className="call-icon">
+      <img src={CallIcon} alt="CallIcon" />
+      </div>
+      <div className="number">
+      <p>Phone: 123-456-7890</p>
+      </div>
         </div>
         <div className="email">
-          <p>info@glamourgallerie.com</p>
+        <div className="mail-icon">
+        <img src={MailIcon} alt="CallIcon" />
+        </div>
+        <div className="mail">
+        <p>info@glamourgallerie.com</p>
+        </div>
+          
         </div>
         <div className="my-account">
-          <p>My Account</p>
+        <div className="person-icon">
+        <img src={PersonIcon} alt="PersonIcon" />
         </div>
+        <div className="account">
+        <p>My Account</p>
+        </div>
+        </div>
+      </div>
+       
       </div>
       <div className="bar">
         <div className="cart">
