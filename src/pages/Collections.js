0dade6cdbc95dec1b7798b3a6ee40606727c3061
CHEGEBB <chegephil24@gmail.com>
2024-04-30 18:12:23 +0300

    scss

--- src/pages/Collections.js
+++ src/pages/Collections.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import LeftSideBar from '../components/LeftSideBar';
 import './Collections.scss';
+import wishlistIcon from '../images/icon-park-solid--love-and-help.svg';
+import cartIcon from '../images/ic--round-shopping-cart.svg';
 
 const Collections = () => {
     const [items, setItems] = useState([
@@ -9,33 +11,50 @@ const Collections = () => {
             name: 'Item 1',
             price: '$10',
             rating: 4,
-            images: ['item1_1.jpg', 'item1_2.jpg', 'item1_3.jpg', 'item1_4.jpg'],
-            wishlistIconPath: 'wishlist_icon.svg',
-            addToCartIconPath: 'cart_icon.svg',
+            images: [
+                require('../images/webp/b1.webp'),
+                require('../images/webp/suit4.webp'),
+                require('../images/webp/suit4.webp'),
+                require('../images/acc4.jpg')
+            ],
+            wishlistIconPath: wishlistIcon,
+            addToCartIconPath:cartIcon,
+            currentImageIndex: 0
         },
         {
             id: 2,
             name: 'Item 2',
             price: '$15',
             rating: 3,
-            images: ['item2_1.jpg', 'item2_2.jpg', 'item2_3.jpg', 'item2_4.jpg'],
+            images: [
+                require('../images/webp/a5.webp'),
+                require('../images/webp/d2.webp'),
+                require('../images/webp/suit3.webp'),
+                require('../images/webp/b5.webp')
+            ],
             wishlistIconPath: 'wishlist_icon.svg',
             addToCartIconPath: 'cart_icon.svg',
+            currentImageIndex: 0
         },
         {
             id: 3,
             name: 'Item 3',
             price: '$20',
             rating: 5,
-            images: ['item3_1.jpg', 'item3_2.jpg', 'item3_3.jpg', 'item3_4.jpg'],
+            images: [
+                require('../images/webp/b3.webp'),
+                require('../images/webp/c5.webp'),
+                require('../images/webp/a3.webp'),  
+                require('../images/webp/suit4.webp')
+            ],
             wishlistIconPath: 'wishlist_icon.svg',
             addToCartIconPath: 'cart_icon.svg',
+            currentImageIndex: 0
         },
         // Add more items as needed
     ]);
 
     useEffect(() => {
-        // Function to cycle through images every 3 seconds for each item
         const interval = setInterval(() => {
             setItems(prevItems =>
                 prevItems.map(item => {
