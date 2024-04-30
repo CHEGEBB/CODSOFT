4729972a85a5f7b330f1d5f893b35b1f186253fd
CHEGEBB <chegephil24@gmail.com>
2024-04-30 19:32:39 +0300

    scss

--- src/components/LeftSideBar.js
+++ src/components/LeftSideBar.js
@@ -35,18 +35,32 @@ const LeftSideBar = () => {
 
     return (
         <div className="sidebar">
+            <div className="categories-bar">
+            <div className="cate">
+            <h3>Categories</h3>
+            </div>
+                <div className="ul">
+                <ul>
+                    <li>Men</li>
+                    <li>Women</li>
+                    <li>Deals and Discounts</li>
+                    <li>Latest</li>
+                </ul>
+                </div>
+                
+            </div>
             <div className="filters">
                 <h3>Filter By</h3>
                 <div className="filter-item">
                     <h4>Availability</h4>
                     <div>
-                        <label>
+                        <label className="square-radio">
                             <input type="radio" name="availability" value="inStock" checked={availability === 'inStock'} onChange={handleAvailabilityChange} />
                             In Stock
                         </label>
                     </div>
                     <div>
-                        <label>
+                        <label className="square-radio">
                             <input type="radio" name="availability" value="outOfStock" checked={availability === 'outOfStock'} onChange={handleAvailabilityChange} />
                             Out of Stock
                         </label>
@@ -58,23 +72,40 @@ const LeftSideBar = () => {
                 </div>
                 <div className="filter-item">
                     <h4>Product Type</h4>
-                    <label>
+                    <label className="square-radio">
                         <input type="radio" name="productType" value="t-shirts" checked={productType === 't-shirts'} onChange={handleProductTypeChange} />
                         T-Shirts
                     </label>
-                    <label>
+                    <label className="square-radio">
                         <input type="radio" name="productType" value="shirts" checked={productType === 'shirts'} onChange={handleProductTypeChange} />
                         Shirts
                     </label>
-                    {/* Add other product types as needed */}
+                    
+                    <label className="square-radio">
+                        <input type="radio" name="productType" value="pants" checked={productType === 'pants'} onChange={handleProductTypeChange} />
+                        Pants 
+                    </label>
+                    <label className="square-radio">
+                        <input type="radio" name="productType" value="shorts" checked={productType === 'shorts'} onChange={handleProductTypeChange} />
+                        Shorts
+                        </label>
+                    <label className="square-radio">
+                        <input type="radio" name="productType" value="shoes" checked={productType === 'shoes'} onChange={handleProductTypeChange} />
+                        Shoes
+                    </label>
+                    <label className="square-radio">
+                        <input type="radio" name="productType" value="accessories" checked={productType === 'accessories'} onChange={handleProductTypeChange} />
+                        Accessories
+                        </label>
+                        <
                 </div>
                 <div className="filter-item">
                     <h4>Brand</h4>
-                    <label>
+                    <label className="square-radio">
                         <input type="radio" name="brand" value="brand1" checked={brand === 'brand1'} onChange={handleBrandChange} />
                         Brand 1
                     </label>
-                    <label>
+                    <label className="square-radio">
                         <input type="radio" name="brand" value="brand2" checked={brand === 'brand2'} onChange={handleBrandChange} />
                         Brand 2
                     </label>
@@ -82,11 +113,11 @@ const LeftSideBar = () => {
                 </div>
                 <div className="filter-item">
                     <h4>Color</h4>
-                    <label>
+                    <label className="square-radio">
                         <input type="radio" name="color" value="red" checked={color === 'red'} onChange={handleColorChange} />
                         Red
                     </label>
-                    <label>
+                    <label className="square-radio">
                         <input type="radio" name="color" value="blue" checked={color === 'blue'} onChange={handleColorChange} />
                         Blue
                     </label>
@@ -94,11 +125,11 @@ const LeftSideBar = () => {
                 </div>
                 <div className="filter-item">
                     <h4>Size</h4>
-                    <label>
+                    <label className="square-radio">
                         <input type="radio" name="size" value="small" checked={size === 'small'} onChange={handleSizeChange} />
                         Small
                     </label>
-                    <label>
+                    <label className="square-radio">
                         <input type="radio" name="size" value="medium" checked={size === 'medium'} onChange={handleSizeChange} />
                         Medium
                     </label>
