1a94fdd2947a3c0f269c4ebda3585ffbcb59ae0e
CHEGEBB <chegephil24@gmail.com>
2024-05-01 16:10:07 +0300

    scss

--- src/pages/Collections.js
+++ src/pages/Collections.js
@@ -432,10 +432,10 @@ const Collections = () => {
       discountedPrice: 112,
       rating: 5,
       images: [
-        require("../images/webp/b3.webp"),
-        require("../images/webp/a3.webp"),
-        require("../images/webp/d2.webp"),
-        require("../images/webp/suit4.webp")
+        require("../images/webp/ms1.jpg"),
+        require("../images/webp/ms2.jpg"),
+        require("../images/webp/ms3.jpg"),
+        require("../images/webp/ms4.jpg")
       ],
       wishlistIconPath: wishlistIcon,
       addToCartIconPath: cartIcon,
