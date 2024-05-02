d109629ebf192bc8d00de2471e125a612492bf75
CHEGEBB <chegephil24@gmail.com>
2024-05-02 13:08:27 +0300

    scss

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Shop.scss';
 import Men from '../pages/categories/men';
 import Women from '../pages/categories/women';
 import Kids from '../pages/categories/kids';
 import Accessories from '../pages/categories/accessories';
-import Shoes from '../pages/categories/';
+import Shoes from '../pages/categories/shoes';
 
 const Shop = () => {
-    return (
-        // nav to select the category of products to display when clicked it will display the products of that category these  will be stored in components 
-        // and will be displayed in the shop page so we will have components for each category of products i.e Accessories,Men, Women, Kids, Shoes, Bags
-        <div className="shop-items">
-            {/* now the nav */}
+    const [selectedCategory, setSelectedCategory] = useState('flashSales');
 
+    const handleCategoryClick = (category) => {
+        setSelectedCategory(category);
+    };
+
+    return (
+        // Navigation to select the category of products to display
+        <div className="shop-page">
+            <div className="categories-nav">
+                <button onClick={() => handleCategoryClick('flashSales')}>Flash Sales</button>
+                <button onClick={() => handleCategoryClick('men')}>Men</button>
+                <button onClick={() => handleCategoryClick('women')}>Women</button>
+                <button onClick={() => handleCategoryClick('kids')}>Kids</button>
+                <button onClick={() => handleCategoryClick('accessories')}>Accessories</button>
+                <button onClick={() => handleCategoryClick('shoes')}>Shoes</button>
+            </div>
+
+            {/* Render products based on selectedCategory */}
+            {selectedCategory === 'flashSales' && (
+                <div className="flash-sales-products">
+                    {/* Render flash sales products */}
+                    Flash Sales
+                </div>
+            )}
+            {selectedCategory === 'men' && <Men />}
+            {selectedCategory === 'women' && <Women />}
+            {selectedCategory === 'kids' && <Kids />}
+            {selectedCategory === 'accessories' && <Accessories />}
+            {selectedCategory === 'shoes' && <Shoes />}
         </div>
     );
 }
