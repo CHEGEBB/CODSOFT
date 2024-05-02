b3041d118069fa03ab09665a67b92f5a00d02eac
CHEGEBB <chegephil24@gmail.com>
2024-05-02 12:57:32 +0300

    scss

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Shop = () => {
     return (
-        <div>
-            <h1>Shop</h1>
-            {/* Shop page content */}
+        // nav to select the category of products to display when clicked it will display the products of that category these  will be stored in components 
+        // and will be displayed in the shop page so we will have components for each category of products i.e Accessories,Men, Women, Kids, Shoes, Bags
+        <div className="shop-items">
+            {/* now the nav */}
+
         </div>
     );
 }
