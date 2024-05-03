cbb8bb00c28b3ffb296cc7f665daa39d4c3958e0
CHEGEBB <chegephil24@gmail.com>
2024-05-03 14:00:48 +0300

    scss

--- src/pages/categories/Flash.js
+++ src/pages/categories/Flash.js
@@ -47,10 +47,10 @@ const Flash = () => {
       discountedPrice: 16,
       rating: 5,
       images: [
-        require("../../images/webp/flash/c1.jpg"),
-        require("../../images/webp/flash/c2.jpg"),
-        require("../../images/webp/flash/c3.jpg"),
-        require("../../images/webp/flash/c4.jpg")
+        require("../../images/webp/flash/e1.jpg"),
+        require("../../images/webp/flash/e2.jpg"),
+        require("../../images/webp/flash/e3.jpg"),
+        require("../../images/webp/flash/e4.jpg")
       ],
       wishlistIconPath: wishlistIcon,
       addToCartIconPath: cartIcon,
@@ -63,10 +63,10 @@ const Flash = () => {
       discountedPrice: 20,
       rating: 4,
       images: [
-        require("../../images/webp/flash/d1.jpg"),
-        require("../../images/webp/flash/d2.jpg"),
-        require("../../images/webp/flash/d3.jpg"),
-        require("../../images/webp/flash/d4.jpg")
+        require("../../images/webp/flash/f1.jpg"),
+        require("../../images/webp/flash/f2.jpg"),
+        require("../../images/webp/flash/f3.jpg"),
+        require("../../images/webp/flash/f4.jpg")
       ],
       wishlistIconPath: wishlistIcon,
       addToCartIconPath: cartIcon,
@@ -79,10 +79,10 @@ const Flash = () => {
       discountedPrice: 24,
       rating: 3,
       images: [
-        require("../../images/webp/a1.webp"),
-        require("../../images/webp/a2.webp"),
-        require("../../images/webp/a4.webp"),
-        require("../../images/webp/a5.webp")
+        require("../../images/webp/flash/c1.jpg"),
+        require("../../images/webp/flash/c2.jpg"),
+        require("../../images/webp/flash/c3.jpg"),
+        require("../../images/webp/flash/c4.jpg")
       ],
       wishlistIconPath: wishlistIcon,
       addToCartIconPath: cartIcon,
