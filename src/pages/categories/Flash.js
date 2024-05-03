1903809faedebb3f8698f6e9dfc2e34e783377c4
CHEGEBB <chegephil24@gmail.com>
2024-05-03 13:55:11 +0300

    scss

--- src/pages/categories/Flash.js
+++ src/pages/categories/Flash.js
@@ -47,10 +47,10 @@ const Flash = () => {
       discountedPrice: 16,
       rating: 5,
       images: [
-        require("../../images/webp/a1.webp"),
-        require("../../images/webp/a2.webp"),
-        require("../../images/webp/a4.webp"),
-        require("../../images/webp/a5.webp")
+        require("../../images/webp/flash/c1.jpg"),
+        require("../../images/webp/flash/c2.jpg"),
+        require("../../images/webp/flash/c3.jpg"),
+        require("../../images/webp/flash/c4.jpg")
       ],
       wishlistIconPath: wishlistIcon,
       addToCartIconPath: cartIcon,
