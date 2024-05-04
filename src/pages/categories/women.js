dca48df86f32b299fc478fe537aac5adbe614fe1
CHEGEBB <chegephil24@gmail.com>
2024-05-04 19:10:13 +0300

    scss

--- src/pages/categories/women.js
+++ src/pages/categories/women.js
@@ -12,10 +12,11 @@ const Women = () => {
       discountedPrice: 8,
       rating: 4,
       images: [
-        require("../../images/webp/a1.webp"),
-        require("../../images/webp/a2.webp"),
-        require("../../images/webp/a4.webp"),
-        require("../../images/webp/a5.webp")
+        require("../../images/webp/women/a1.jpg"),
+        require("../../images/webp/women/a2.jpg"),
+        require("../../images/webp/women/a3.jpg"),
+        require("../../images/webp/women/a4.jpg"),
+
       ],
       wishlistIconPath: wishlistIcon,
       addToCartIconPath: cartIcon,
