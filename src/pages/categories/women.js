854ecbd3676473e2a585eb845a2e71820bda30cd
CHEGEBB <chegephil24@gmail.com>
2024-05-04 19:50:44 +0300

    scss

--- src/pages/categories/women.js
+++ src/pages/categories/women.js
@@ -293,10 +293,10 @@ const Women = () => {
       discountedPrice: 8,
       rating: 4,
       images: [
-        require("../../images/webp/a1.webp"),
-        require("../../images/webp/a2.webp"),
-        require("../../images/webp/a4.webp"),
-        require("../../images/webp/a5.webp")
+        require("../../images/webp/women/r1.jpg"),
+        require("../../images/webp/women/r2.jpg"),
+        require("../../images/webp/women/r3.jpg"),
+        require("../../images/webp/women/r4.jpg")
       ],
       wishlistIconPath: wishlistIcon,
       addToCartIconPath: cartIcon,
@@ -309,10 +309,10 @@ const Women = () => {
       discountedPrice: 8,
       rating: 4,
       images: [
-        require("../../images/webp/a1.webp"),
-        require("../../images/webp/a2.webp"),
-        require("../../images/webp/a4.webp"),
-        require("../../images/webp/a5.webp")
+        require("../../images/webp/women/s1.jpg"),
+        require("../../images/webp/women/s2.jpg"),
+        require("../../images/webp/women/s3.jpg"),
+        require("../../images/webp/women/s4.jpg")
       ],
       wishlistIconPath: wishlistIcon,
       addToCartIconPath: cartIcon,
