b0379d67dd0e40a666cc5e4afd8b9a6ccc3fc3c7
CHEGEBB <chegephil24@gmail.com>
2024-05-04 20:23:14 +0300

    scss

--- src/pages/categories/women.js
+++ src/pages/categories/women.js
@@ -489,7 +489,7 @@ const Women = () => {
                       <span
                         key={index}
                         className="star-icon"
-                        style={{ color: "crimson", fontSize: "1.5em" }}
+                        style={{ color: "#BDB76B", fontSize: "1.5em" }}
                       >
                         ★
                       </span>
