d0fcf64232c466864ade807c099b1edd1a31629b
CHEGEBB <chegephil24@gmail.com>
2024-05-05 10:56:26 +0300

    scss

--- src/pages/categories/kids.js
+++ src/pages/categories/kids.js
@@ -312,7 +312,7 @@ const Kids = () => {
                     </div>
                   </div>
                 </div>
-                <div className="item-info">
+                <div className="item-info-kids">
                   <div className="item-name">{item.name}</div>
                   <div className="price-container">
                     <span className="previous-price">${item.price}</span>
