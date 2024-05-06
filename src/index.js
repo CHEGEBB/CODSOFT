7ad6d510d4e9e90664893b2a4ca044996f0e1f84
CHEGEBB <chegephil24@gmail.com>
2024-05-06 19:34:54 +0300

    scss

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.scss';
 import App from './App';
+import Auth from './Auth';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Auth />
   </React.StrictMode>
 );
