3bdcaa227e0cd7cd0c93c80213ac99e8a8f50fba
CHEGEBB <chegephil24@gmail.com>
2024-05-07 11:31:17 +0300

    scss

--- src/auth-pages/Signup.js
+++ src/auth-pages/Signup.js
@@ -2,11 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Signup.scss";
 
+
 const SignUp = () => {
   return (
     <div className="signup-container">
       <div className="left-container">
-      
+
       </div>
       <div className="right-container">
         <nav className="horizontal-nav">
