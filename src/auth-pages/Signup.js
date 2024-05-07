6d86b5ecb831b9a9c95609c9dbb91daff9c12698
CHEGEBB <chegephil24@gmail.com>
2024-05-07 12:57:51 +0300

    scss

--- src/auth-pages/Signup.js
+++ src/auth-pages/Signup.js
@@ -1,55 +1,61 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "./Signup.scss";
 import Background from "../images/webp/women/Background.mp4";
-
 
 const SignUp = () => {
   return (
     <div className="signup-container">
-    <div className="overlay"></div>
-    <div className="card">
-      
-      <div className="left">
-      <div className="left-container">
-        <nav className="horizontal-nav">
-          <ul>
-            <li><Link to="/signup">Sign Up</Link></li>
-            <li><Link to="/">Login</Link></li>
-          </ul>
-        </nav>
-        <div className="signup-form">
-          <h2>Create an Account</h2>
-          <form>
-            {/* Form fields */}
-            <div className="form-group">
-              <label htmlFor="email">Email:</label>
-              <input type="email" id="email" name="email" />
+      <div className="overlay"></div>
+      <div className="card">
+        <div className="left">
+          <div className="left-container">
+            <nav className="horizontal-nav">
+              <ul>
+                <li>
+                  <NavLink exact to="/signup" activeClassName="active">
+                    Sign Up
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink exact to="/" activeClassName="active">
+                    Login
+                  </NavLink>
+                </li>
+              </ul>
+            </nav>
+            <div className="signup-form">
+              <h2>Create an Account</h2>
+              <form>
+                {/* Form fields */}
+                <div className="form-group">
+                  <label htmlFor="email">Email:</label>
+                  <input type="email" id="email" name="email" />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="password">Password:</label>
+                  <input type="password" id="password" name="password" />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="confirmPassword">Confirm Password:</label>
+                  <input type="password" id="confirmPassword" name="confirmPassword" />
+                </div>
+                {/* Submit button */}
+                <button type="submit">Sign Up</button>
+              </form>
+              <div className="login-link">
+                <p>Already have an account? <Link to="/">Login</Link></p>
+              </div>
             </div>
-            <div className="form-group">
-              <label htmlFor="password">Password:</label>
-              <input type="password" id="password" name="password" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="confirmPassword">Confirm Password:</label>
-              <input type="password" id="confirmPassword" name="confirmPassword" />
-            </div>
-            {/* Submit button */}
-            <button type="submit">Sign Up</button>
-          </form>
-          <div className="login-link">
-            <p>Already have an account? <Link to="/">Login</Link></p>
           </div>
         </div>
-      </div>
-      </div>
-      <div className="right">
-      <div className="overlay-2">
-      <video autoPlay muted loop>
-        <source src={Background} type="video/mp4" />
-      </video>
-      </div>
-      </div>
+        <div className="right">
+          <div className="overlay-2">
+            <video autoPlay muted loop>
+              <source src={Background} type="video/mp4" />
+            </video>
+          </div>
+        </div>
       </div>
     </div>
   );
