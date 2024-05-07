ffb82d25da6d1faec7cfa439cccba5a8c3df56f2
CHEGEBB <chegephil24@gmail.com>
2024-05-07 09:27:11 +0300

    scss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import Cart from "./pages/Cart";
@@ -8,89 +7,16 @@ import Collections from "./pages/Collections";
 import Contact from "./pages/Contact";
 import Shop from "./pages/Shop";
 import User from "./pages/User";
-import CartIcon from "./images/ic--round-shopping-cart.svg";
-import Wishlist from "./images/icon-park-solid--love-and-help.svg";
-import SearchIcon from "./images/ant-design--search-outlined.svg";
-import CallIcon from "./images/ic--round-call.svg";
-import MailIcon from "./images/mdi--email.svg";
-import PersonIcon from "./images/mdi--person.svg";
-import bg from "./images/webp/kids/bg3.mp4";
 import "./index.scss";
+import Login from "./auth-pages/Login";
 
 
 const App = () => {
   return (
     <Router>
-      <div className="header">
-        <div className="reach">
-          <div className="phone">
-            <div className="call-icon">
-              <img src={CallIcon} alt="CallIcon" />
-            </div>
-            <div className="number">
-              <p>Phone: 123-456-7890</p>
-            </div>
-          </div>
-          <div className="email">
-            <div className="mail-icon">
-              <img src={MailIcon} alt="MailIcon" />
-            </div>
-            <div className="mail">
-              <p>info@glamourgallerie.com</p>
-            </div>
-          </div>
-          <div className="my-account">
-            <div className="person-icon">
-              <img src={PersonIcon} alt="PersonIcon" />
-            </div>
-            <div className="account">
-              <p>My Account</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="bar">
-        <div className="choice">
-          <div className="cart">
-            <div className="catcon">
-              <img src={CartIcon} alt="Cart Icon" />
-            </div>
-            <div className="cart-content">
-              <h1>Cart</h1>
-            </div>
-          </div>
-          <div className="wishlist">
-            <div className="wishcon">
-              <img src={Wishlist} alt="Wishlist Icon" />
-            </div>
-            <div className="wishlist-content">
-              <h1>Wishlist</h1>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="search-container">
-      {/* Video with overlay as background */}
-      <div className="video-container-bg">
-          <video autoPlay loop muted className="background-video">
-            <source src={bg} type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-          <div className="overlay-bg"></div> {/* Overlay */}
-          <input
-            type="text"
-            className="input-search"
-            placeholder="Search for products here..."
-          />
-          <span className="filter-icon"></span>
-          <button type="submit">
-            <img src={SearchIcon} alt="Search Icon" />
-          </button>
-        </div>
-      </div>
-      <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/products" element={<Products />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/collections" element={<Collections />} />
--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.scss';
 import App from './App';
-import Auth from './Auth';
+import Login from './auth-pages/Login';
+
+// Example isAuthenticated logic
+const isAuthenticated = true; // Change this based on your actual authentication logic
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {isAuthenticated ? <App /> : <Login />}
   </React.StrictMode>
 );
