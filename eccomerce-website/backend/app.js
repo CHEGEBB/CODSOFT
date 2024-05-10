1f7e8e926901ef8cd7df84cdf99c0f3483f60145
CHEGEBB <chegephil24@gmail.com>
2024-05-10 14:09:33 +0300

    server

--- eccomerce-website/backend/app.js
+++ eccomerce-website/backend/app.js
@@ -4,3 +4,13 @@ const http = require('http');
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
+app.get('./', (req,res) => {
+    res.send('Hello World');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
