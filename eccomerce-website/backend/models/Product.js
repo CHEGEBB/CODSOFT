50b0b339790a532ed19f586311179a224fc069e2
CHEGEBB <chegephil24@gmail.com>
2024-05-11 12:31:58 +0300

    filtering

---
+++ eccomerce-website/backend/models/Product.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    discountedPrice: { type: Number, required: true },
+    rating: { type: Number, required: true },
+    images: [{ type: String, required: true }],
+    wishlistIconPath: { type: String, required: true },
+    addToCartIconPath: { type: String, required: true },
+    currentImageIndex: { type: Number, required: true }
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
