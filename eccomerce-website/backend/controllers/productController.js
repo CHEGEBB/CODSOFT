c80c8fa1a43b43bf431d921e5fac4c6f950f4bfb
CHEGEBB <chegephil24@gmail.com>
2024-05-11 12:32:52 +0300

    filter

---
+++ eccomerce-website/backend/controllers/productController.js
@@ -0,0 +1,26 @@
+// controllers/productController.js
+
+const Product = require('../models/product');
+
+// Controller function to search products
+exports.searchProducts = async (req, res, next) => {
+  try {
+    const { query } = req.query;
+    const products = await Product.find({ $text: { $search: query } });
+    res.json(products);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Controller function to filter products
+exports.filterProducts = async (req, res, next) => {
+  try {
+    // Example filtering: filtering products with price greater than or equal to a certain value
+    const { minPrice } = req.query;
+    const products = await Product.find({ price: { $gte: minPrice } });
+    res.json(products);
+  } catch (error) {
+    next(error);
+  }
+};
