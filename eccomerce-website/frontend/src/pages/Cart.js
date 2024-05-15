de09f59f5f603b9487e0a2c3b87d313abe79a449
CHEGEBB <chegephil24@gmail.com>
2024-05-15 13:20:53 +0300

    backend

--- eccomerce-website/frontend/src/pages/Cart.js
+++ eccomerce-website/frontend/src/pages/Cart.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
-
-// Importing a sample image for reference
-import image from '../images/webp/men/k1.jpg';
+import './Cart.scss'; // Import your custom styles for Cart
 
 const Cart = () => {
   // Use the useLocation hook to get the current location
@@ -11,30 +9,17 @@ const Cart = () => {
   // Extract the fetched item from the location state safely
   const fetchedItem = location.state && location.state.item;
 
-  console.log(fetchedItem);
-
-  // Define a function to extract the desired part of the image URL
-  const getImageUrl = (imageUrl, category) => {
-    // Extract the filename from the image URL
-    const filename = imageUrl.split('/').pop();
-    // Extract the desired part (e.g., 'm1') from the filename
-    const desiredPart = filename.split('.')[0];
-    // Construct the new image URL based on the category and the desired part
-    return `../images/webp/${category}/${desiredPart}.jpg`;
-  };
-
   return (
-    <div>
+    <div className="cart-container">
       <h1>Cart</h1>
       {/* Display the fetched item */}
       {fetchedItem && (
-        <div>
-          <h2>Selected Item</h2>
-          <p>Name: {fetchedItem.name}</p>
-          <p>Price: ${fetchedItem.price}</p>
-          <p>Category: {fetchedItem.category}</p>
-          {/* Display only one image */}
-          <img src={getImageUrl(fetchedItem.images[0], fetchedItem.category)} alt={fetchedItem.name} />
+        <div className="product-details">
+          <div className="product-info">
+            <p><strong>Name:</strong> {fetchedItem.name}</p>
+            <p><strong>Price:</strong> ${fetchedItem.price}</p>
+            <p><strong>Category:</strong> {fetchedItem.category}</p>
+          </div>
         </div>
       )}
     </div>
