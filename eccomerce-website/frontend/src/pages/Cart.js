333f14a7d0fa9d2678d76935cdd83f6206e0b4ad
CHEGEBB <chegephil24@gmail.com>
2024-05-15 14:08:42 +0300

    backend

--- eccomerce-website/frontend/src/pages/Cart.js
+++ eccomerce-website/frontend/src/pages/Cart.js
@@ -1,9 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
-import './Cart.scss'; // Import your custom styles for Cart
 
 const Cart = () => {
-  // Initialize state to store cart items
   const [cartItems, setCartItems] = useState([]);
 
   // Use the useLocation hook to get the current location
@@ -21,14 +19,27 @@ const Cart = () => {
     // Filter out the item with the specified ID from the cartItems array
     const updatedCartItems = cartItems.filter(item => item._id !== itemId);
     setCartItems(updatedCartItems);
+    // Update local storage after deleting item
+    localStorage.setItem('cartItems', JSON.stringify(updatedCartItems));
   };
 
   // Add the fetched item to the cartItems array
-  const addToCart = () => {
-    if (location.state && location.state.item) {
-      setCartItems([...cartItems, location.state.item]);
+  useEffect(() => {
+    // Retrieve cart items from local storage
+    const storedCartItems = localStorage.getItem('cartItems');
+    if (storedCartItems) {
+      setCartItems(JSON.parse(storedCartItems));
     }
-  };
+    // Add the new item to cart if present in location state
+    if (location.state && location.state.item) {
+      setCartItems(prevCartItems => [...prevCartItems, location.state.item]);
+    }
+  }, [location.state]);
+
+  // Update local storage whenever cartItems change
+  useEffect(() => {
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+  }, [cartItems]);
 
   return (
     <div className="cart-container">
@@ -51,10 +62,8 @@ const Cart = () => {
           <button className="checkout-button" onClick={handleCheckout}>Checkout</button>
         </div>
       ) : (
-        <p>Click the button below to see your items.</p>
+        <p>Your cart is empty</p>
       )}
-      {/* Add to cart button */}
-      <button className="add-to-cart-button" onClick={addToCart}>View Items</button>
     </div>
   );
 };
