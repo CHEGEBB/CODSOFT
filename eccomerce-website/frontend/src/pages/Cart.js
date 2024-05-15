aab8bb044c4c079804dc216d0b7f4effac760161
CHEGEBB <chegephil24@gmail.com>
2024-05-15 10:50:07 +0300

    new

--- eccomerce-website/frontend/src/pages/Cart.js
+++ eccomerce-website/frontend/src/pages/Cart.js
@@ -1,16 +1,23 @@
-// In your Cart.js component
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const Cart = () => {
+const Cart = ({ location }) => {
   const [cartItems, setCartItems] = useState([]);
 
   useEffect(() => {
-    fetchCartItems();
-  }, []);
+    // Check if item data is passed from Men.js
+    if (location.state && location.state.item) {
+      // If item data is passed, add it to cartItems state
+      setCartItems([location.state.item]);
+    } else {
+      // If no item data is passed, fetch cart items from the backend
+      fetchCartItems();
+    }
+  }, [location.state]);
 
   const fetchCartItems = async () => {
     try {
+      // Fetch cart items from the backend
       const { data } = await axios.get('http://localhost:3000/cart/cart-items');
       setCartItems(data);
     } catch (error) {
@@ -26,6 +33,7 @@ const Cart = () => {
           <div key={item._id}>
             <p>Name: {item.name}</p>
             <p>Price: ${item.discountedPrice}</p>
+            {/* Add other item details */}
             <img src={item.image} alt={item.name} />
           </div>
         ))}
