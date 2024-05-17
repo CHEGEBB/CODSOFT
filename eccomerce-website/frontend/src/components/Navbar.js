a344aa8ca8f9c3c8a48bd8d6fee211149911ffb7
CHEGEBB <chegephil24@gmail.com>
2024-05-17 14:18:54 +0300

    navbar

--- eccomerce-website/frontend/src/components/Navbar.js
+++ eccomerce-website/frontend/src/components/Navbar.js
@@ -1,36 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import CategoryIcon from "../images/ic--baseline-apps.svg";
+import "./Navbar.scss";
 
 const Navbar = () => {
     return (
-        <nav className="navbar">
+        <nav className="navbar-main">
             <div className="container">
-            <div className="categories">
-            <span className="category-icon">
-            <img src={CategoryIcon} alt="Category Icon" />
-            <h2>Categories</h2>
-            </span>
-        </div>
-            <div className="container2">
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <Link to="/home" className="nav-link">Home</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/products" className="nav-link">Products</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/collections" className="nav-link">Collections</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/shop" className="nav-link">Shop</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/contact" className="nav-link">Contact</Link>
-                    </li>
-                </ul>
-            </div>
+                <div className="categories">
+                    <span className="category-icon">
+                        <img src={CategoryIcon} alt="Category Icon" />
+                        <h2>Categories</h2>
+                    </span>
+                </div>
+                <div className="container2">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link to="/home" className="nav-link">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/products" className="nav-link">Products</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/collections" className="nav-link">Collections</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/shop" className="nav-link">Shop</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/contact" className="nav-link">Contact</Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     );
