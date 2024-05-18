1a952dad266150ed17283fc24cfbdbf463864581
CHEGEBB <chegephil24@gmail.com>
2024-05-18 03:03:07 -0500

    new

--- Project-management-tool/frontend/src/App.jsx
+++ Project-management-tool/frontend/src/App.jsx
@@ -2,14 +2,20 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/Homepage';
 import ProjectPage from './pages/ProjectPage';
+import Sidenav from './components/Sidenav'
+
 
 const App = () => {
     return (
         <Router>
+        <div className="app">
+
             <Routes>
+
                 <Route path="/" element={<HomePage />} />
                 <Route path="/project" element={<ProjectPage />} />
             </Routes>
+            </div>
         </Router>
     );
 };
