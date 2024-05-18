b4f108ffb359e6d33df5b6e3a374d46f2111db48
CHEGEBB <chegephil24@gmail.com>
2024-05-18 03:12:00 -0500

    scss

--- Project-management-tool/frontend/src/components/Sidenav.jsx
+++ Project-management-tool/frontend/src/components/Sidenav.jsx
@@ -1,13 +1,14 @@
 // src/components/SideNav.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Sidenav.scss'
 
 const SideNav = () => {
     return (
         <nav className="side-nav">
             <ul>
                 <li><Link to="/">Home</Link></li>
-                {/* Add other navigation links here */}
+                <li><Link to ="/project">Project</Link></li>
             </ul>
         </nav>
     );
