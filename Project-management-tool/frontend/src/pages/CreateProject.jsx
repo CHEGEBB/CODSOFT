bc77da2085d55bdca8c680e8772e8055607da88c
CHEGEBB <chegephil24@gmail.com>
2024-05-20 16:00:33 +0300

    new

--- Project-management-tool/frontend/src/pages/CreateProject.jsx
+++ Project-management-tool/frontend/src/pages/CreateProject.jsx
@@ -1,10 +1,33 @@
-import './Project.scss'
-const ProjectPage = () => {
+
+const CreateProject = () => {
     return ( 
-        <div className="project">
-        <h1>Projectpage</h1>
+        <div className="project-create">
+            <h1>Create Project</h1>
+            <form>
+                <div className="form-group">
+                    <label htmlFor="projectName">Project Name</label>
+                    <input type="text" id="projectName" name="projectName" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="projectDescription">Project Description</label>
+                    <textarea id="projectDescription" name="projectDescription"></textarea>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="projectManager">Project Manager</label>
+                    <input type="text" id="projectManager" name="projectManager" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="projectMembers">Project Members</label>
+                    <input type="text" id="projectMembers" name="projectMembers" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="projectDeadline">Project Deadline</label>
+                    <input type="date" id="projectDeadline" name="projectDeadline" />
+                </div>
+                <button type="submit">Create Project</button>
+            </form>
         </div>
      );
 }
  
-export default ProjectPage;
+export default CreateProject;
